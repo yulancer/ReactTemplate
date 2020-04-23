17cea93799e06c2ee33dd15c2be71858ef250b22
Yuriy <yulancer@yandex.ru>
2020-04-23 18:04:57 +0300

    запущена доска

--- WebApplication/ClientApp/src/App.js
+++ WebApplication/ClientApp/src/App.js
@@ -34,6 +34,7 @@ export default class App extends Component {
                   <Switch>
                       <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
                       <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+                      <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
                       <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
                       <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
                       <Route path="/" name="Home" render={props => <DefaultLayout {...props} />} />
