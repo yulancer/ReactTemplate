0ffc14d4755770c904b208efd1de81b73ac07cf4
Yuriy <yulancer@yandex.ru>
2020-04-23 17:47:08 +0300

    dashboard загрузился во view

--- WebApplication/ClientApp/src/App.js
+++ WebApplication/ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+// import { Route } from 'react-router';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
@@ -11,17 +12,40 @@ import { ApplicationPaths } from './components/api-authorization/ApiAuthorizatio
 import './custom.css'
 import './App.scss';
 
+
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/Pages/Login'));
+const Register = React.lazy(() => import('./views/Pages/Register'));
+const Page404 = React.lazy(() => import('./views/Pages/Page404'));
+const Page500 = React.lazy(() => import('./views/Pages/Page500'));
+
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
+      return (
+          <HashRouter>
+              <React.Suspense fallback={loading()}>
+                  <Switch>
+                      <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+                      <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+                      <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
+                      <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
+                      <Route path="/" name="Home" render={props => <DefaultLayout {...props} />} />
+                  </Switch>
+              </React.Suspense>
+          </HashRouter>
+      // <Layout>
+      //  <Route exact path='/' component={Home} />
+      //  <Route path='/counter' component={Counter} />
+      //  <AuthorizeRoute path='/fetch-data' component={FetchData} />
+      //  <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+      // </Layout>
     );
   }
 }
